957696a3d1995723beae3b3e451797826cf52e81
Anamika Sawhney <anamika.sawhney22@gmail.com>
2024-08-02 10:39:15 +0530

    Add project files.

---
+++ SearchingDemos/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace SearchingDemos
+{
+    internal class Program
+    {
+        static int[] num = [2, 4, 5, 10, 12, 20, 9, 30, 89];
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter element to search");
+            int x = byte.Parse(Console.ReadLine());
+            //bool found = LinearSearch(x);
+            bool found = BinarySearch(x);
+            // unary operator  ++ --  n++
+            /// binary operator   + -  9+7
+            //if (found == true) Console.WriteLine("element found");
+            //else Console.WriteLine("element not found");
+            // ?:  // conditional / ternary operator
+            Console.WriteLine(found == true ? "element found" : "element not found") ;
+        }
+        static bool LinearSearch(int x)
+        {
+            bool flag = false;
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] == x)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            return flag;
+        }
+        static bool BinarySearch(int x)
+        {
+            Array.Sort(num);
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    Console.WriteLine(num[i]);
+            //}
+            bool flag = false;
+            int low = 0;
+            int high = num.Length - 1;
+            int mid = (low + high) / 2;
+            while (low <= high)
+            {
+                if (num[mid] == x)
+                {
+                    flag = true;
+                    break;
+                }
+                else if (x < num[mid])
+                {
+                    high = mid - 1;
+                }
+                else if (x > num[mid])
+                {
+                    low = mid + 1;
+                }
+                mid = (low + high) / 2;
+            }
+
+            return flag;
+
+
+            }
+    }
+}
---
+++ SortingDemos/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace SortingDemos
+{
+    internal class Program
+    {
+        static int[] num = [2, 4, 5, 10, 12, 20, 9, 30, 89];
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Original Array");
+            DisplayElements();
+            //SelectionSort();
+            BubbleSort();
+            Console.WriteLine("after sort");
+            DisplayElements();
+        }
+        static void DisplayElements()
+        {
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void SelectionSort()
+        {
+            int temp;
+            for (int i = 0; i < num.Length - 1; i++)
+            {
+                for (int j = i + 1; j < num.Length; j++)
+
+                {
+                    if (num[i] > num[j])
+                    {
+                        temp = num[i];
+                        num[i] = num[j];
+                        num[j] = temp;
+                    }
+                }
+
+            }
+
+        }
+        static void BubbleSort()
+        {
+            int n = num.Length;
+            int temp;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (num[j] > num[j + 1])
+                    {
+                        // swap elements
+                        temp = num[j];
+                        num[j] = num[j + 1];
+                        num[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        static void InsertionSort()
+        {
+            int x, j = 0;
+            for(int i=0;i<num.Length; i++)
+            {
+                x = num[i];
+                j = i - 1;
+                while(j>=0 && num[j]>x)
+                {
+                    num[j + 1] = num[j];
+                    j = j - 1;
+                }
+                num[j+1] = x;
+            }
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
